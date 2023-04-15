306aace901a9d8df9e2c6f0a6f0870a2336fbd8f
FaTeMe-J <126141630+FaTeMe-J@users.noreply.github.com>
2023-04-16 02:09:02 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number1:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number2:");
+            int b = int.Parse(Console.ReadLine());
+            int flag = 0, z = 0, i = 1;
+            while (flag != 1)
+            {
+                z = a * i;
+                if (z % b == 0)
+                    flag = 1;
+                i++;
+            }
+            Console.WriteLine("K.m.m ={0}", z);
+            while (b != 0)
+            {
+                int c = a % b;
+                if (c == 0)
+                    Console.WriteLine("B.m.m={0}", b);
+                else
+                    a = b;
+                b = c;
+            }
+        }
+    }
+}
