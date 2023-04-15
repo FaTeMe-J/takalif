5140f2726b982a10e5c7035844ba4d84c8ed4926
FaTeMe-J <126141630+FaTeMe-J@users.noreply.github.com>
2023-04-16 02:20:30 +0330

    Add files via upload
    xcx

--- Program.cs
+++ Program.cs
@@ -1,28 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 
-namespace box_info
-
+namespace T1
 {
-    class test1
-
+    class Test1
     {
-
-        public static void Main()
-
+        static void Main()
         {
-            int x=Convert.ToInt32(Console.ReadLine());
-
-             calc(x);
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Even(x);
+            }
+            else { odd(x); }
         }
-        static void calc(int x)
+        static int Even(int x)
         {
-            double result = Math.Pow(x, 2) + (2 * x) - 4;
-            Console.WriteLine(result); 
+            int sum = 0, zarb = 1;
+            for (int i = 2; i < 2; i++)
+            {
+                sum += i;
+                zarb *= i;
+                Console.WriteLine("majmoe" + sum);
+                Console.WriteLine("haselzarb" + zarb);
+            }
         }
+            static int odd(int x)
+            {
+                int sum = 0, zarb = 1;
+                for (int i = 1; i <= x; i++)
+                {
+                    sum += i;
+                    zarb *= i;
+                    Console.WriteLine("majmoe" + sum);
+                    Console.WriteLine("haselzarb" + zarb);
+                }
+            }
+        
     }
 }
