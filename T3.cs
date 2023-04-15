cd9561fc8a58e0c036e9a4da1c3daff369b825b0
FaTeMe-J <126141630+FaTeMe-J@users.noreply.github.com>
2023-04-16 02:27:56 +0330

    Add files via upload

---
+++ T3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x=Convert.ToInt32(Console.ReadLine());
+            int y=Convert.ToInt32(Console.ReadLine());
+            calc(x, y);
+        }//end main
+        static void calc(int x, int y)
+        {
+            double result = Math.Sqrt(Math.Abs(x) + Math.Pow(y,x));
+            Console.WriteLine(result);
+        }
+    }
+}
