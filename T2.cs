a9b466f9b6edd9141bc4dd6e70d722230dc179d2
FaTeMe-J <126141630+FaTeMe-J@users.noreply.github.com>
2023-04-16 02:26:18 +0330

    Add files via upload

---
+++ T2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace box_info
+
+{
+    class test1
+
+    {
+
+        public static void Main()
+
+        {
+            int x=Convert.ToInt32(Console.ReadLine());
+
+             calc(x);
+        }
+        static void calc(int x)
+        {
+            double result = Math.Pow(x, 2) + (2 * x) - 4;
+            Console.WriteLine(result); 
+        }
+    }
+}
