c2e7a381de11301c51e4759f28d0fa2b89c8caa7
FaTeMe-J <126141630+FaTeMe-J@users.noreply.github.com>
2023-04-28 09:52:43 +0330

    Add files via upload

---
+++ J5-1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J5_1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[50, 50];
+
+            // fill the array with random values
+            Random rand = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    arr[i, j] = rand.Next(100);
+                }
+            }
+
+            // find the median of the array
+            int median = QuickSelect(arr, 0, 49, 24);
+
+            Console.WriteLine("The median of the array is {0}", median);
+
+            Console.ReadKey();
+        }
+
+        static int QuickSelect(int[,] arr, int left, int right, int k)
+        {
+            if (left == right)
+            {
+                return arr[left, k];
+            }
+
+            int pivotIndex = left + (right - left) / 2;
+            pivotIndex = Partition(arr, left, right, pivotIndex, k);
+
+            if (k == pivotIndex)
+            {
+                return arr[pivotIndex, k];
+            }
+            else if (k < pivotIndex)
+            {
+                return QuickSelect(arr, left, pivotIndex - 1, k);
+            }
+            else
+            {
+                return QuickSelect(arr, pivotIndex + 1, right, k);
+            }
+        }
+
+        static int Partition(int[,] arr, int left, int right, int pivotIndex, int k)
+        {
+            int pivotValue = arr[pivotIndex, k];
+            Swap(arr, pivotIndex, right);
+
+            int storeIndex = left;
+            for (int i = left; i < right; i++)
+            {
+                if (arr[i, k] < pivotValue)
+                {
+                    Swap(arr, i, storeIndex);
+                    storeIndex++;
+                }
+            }
+
+            Swap(arr, storeIndex, right);
+
+            return storeIndex;
+        }
+
+        static void Swap(int[,] arr, int i, int j)
+        {
+            for (int k = 0; k < 50; k++)
+            {
+                int temp = arr[i, k];
+                arr[i, k] = arr[j, k];
+                arr[j, k] = temp;
+            }
+        }
+    }
+}
---
+++ J5-2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace J5_2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a 3x3 matrix
+            Matrix<double> matrix = Matrix<double>.Build.DenseOfArray(new double[,] {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        });
+
+            // calculate the determinant of the matrix
+            double det = matrix.Determinant();
+
+            Console.WriteLine("The determinant of the matrix is {0}", det);
+
+            // calculate the inverse of the matrix
+            Matrix<double> inverse = matrix.Inverse();
+
+            Console.WriteLine("The inverse of the matrix is:");
+            Console.WriteLine(inverse);
+
+            Console.ReadKey();
+        }
+    }
+
+}
